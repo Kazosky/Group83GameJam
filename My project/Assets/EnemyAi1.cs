1d0e36a4be09f1ee6d4aa9753b6c6a6de7b477aa
Kazosky <tomwells1806@gmail.com>
2023-09-26 16:52:22 +0100

    Added Enemy health, begun work on different enemy types

--- My project/Assets/EnemyAi1.cs
+++ My project/Assets/EnemyAi1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -8,43 +9,63 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject Enemy;
     public GameObject Player;
     private float range;
-    public float enemySpeed;
     private Rigidbody2D rigidbody;
     private Vector2 targetVelocity = Vector2.zero;
     public float speed = 20f;
     public float maxForce = 20f;
+    public float enemyMaxHealth = 10f;
+    public float enemyCurrentHealth;
+    public bool collidedwPlayer;
 
     void Start()
     {
         Enemy = GameObject.FindGameObjectWithTag("Enemy");
         Player = GameObject.FindGameObjectWithTag("Player");
         rigidbody = GetComponent<Rigidbody2D>();
-        enemySpeed = 5f;
+        enemyCurrentHealth = enemyMaxHealth;
+        collidedwPlayer = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
-        //Vector2 velocity = new Vector2((transform.position.x - Player.transform.position.x) * enemySpeed, (transform.position.y - Player.transform.position.y) * enemySpeed);
-        //GetComponent<Rigidbody2D>().velocity = -velocity;
+    }
 
-        //if (range >= 10f)
-        //{
-        //    enemySpeed = 5f;
-            
-        //}
-
-        //if (range < 10f)
-        //{
-        //    enemySpeed = 2f;
-        //}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collidedwPlayer = true;
+        }
     }
 
     private void FixedUpdate()
     {
+        range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
         targetVelocity = (Player.transform.position - transform.position).normalized * speed;
         Vector2 appliedImpulse = Vector2.ClampMagnitude((targetVelocity - rigidbody.velocity) * rigidbody.mass, maxForce * Time.fixedDeltaTime);
         rigidbody.AddForce(appliedImpulse, ForceMode2D.Impulse);
+
+        
+
+        if (range >= 10f)
+        {
+            speed = 10f;
+        } else if (range < 10f)
+        {
+            speed = 4f;
+        }
+
+        if (collidedwPlayer)
+        {
+            enemyCurrentHealth -= 1f;
+        }
+
+        if (enemyCurrentHealth <= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+       
     }
 }
