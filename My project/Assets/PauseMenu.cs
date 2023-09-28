46d29284256472b2d4ffe8e4e1e91974b86d005a
Jack Lonergan <99100073+JackTheDevFMD@users.noreply.github.com>
2023-09-28 15:03:20 +0100

    UI Update 2

--- My project/Assets/PauseMenu.cs
+++ My project/Assets/PauseMenu.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class PauseMenu : MonoBehaviour
 {
@@ -12,21 +11,28 @@ public class PauseMenu : MonoBehaviour
     public GameObject upgradesMenuUI;
 
     // Upgrade screen parts
+
+    public TMP_Text upgradePoints;
+
     // Upgrade 1:
     public Slider slider1;
     public TMP_Text slider1Text;
+    public GameObject upgradeButton1;
 
     // Upgrade 2:
     public Slider slider2;
     public TMP_Text slider2Text;
+    public GameObject upgradeButton2;
 
     // Upgrade 3:
     public Slider slider3;
     public TMP_Text slider3Text;
+    public GameObject upgradeButton3;
 
     // Wave UI
     public TMP_Text waveText;
 
+    // Scene name to check if the main game is active. Change if scene name is changed. \\
     public static string sceneName = "SampleScene";
 
     void Start()
@@ -40,31 +46,26 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if (SceneManager.GetActiveScene().name == sceneName)
         {
-            if (GameIsPaused)
+
+            // Checks for when the escape key is pressed to pause the game.
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Resume();
-            } else
-            {
-                Pause();
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
             }
-        }
 
-        if (upgradesMenuUI.activeSelf)
-        {
-            // Sets the text variables to the value of the slider corrisponding to that variable.
-            slider1Text.text = slider1.value.ToString();
-            slider2Text.text = slider2.value.ToString();
-            slider3Text.text = slider3.value.ToString();
-        }
-
-        if (SceneManager.GetActiveScene().name == sceneName && GameIsPaused == false) 
-        {
-            waveText.enabled = true;
-        } else
-        {
-            waveText.enabled = false;
+            if (!waveText.IsActive() && !upgradesMenuUI.activeSelf)
+            {
+                ShowWaves();
+            }
         }
     }
 
@@ -73,6 +74,8 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        upgradesMenuUI.SetActive(false);
+        ShowWaves();
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
@@ -83,8 +86,6 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-
-
 
     public void loadMenu()
     {
@@ -113,13 +114,86 @@ public class PauseMenu : MonoBehaviour
         waveText.enabled = false;
     }
 
-
     public void WaveCounterUpdate()
     {
+        // Update the wave counter +1 each time called
+
+
         int currentWaveNumber;
         // Gets the last 2 digits of the wave to reduce the amount of selection required.
         int.TryParse(waveText.text.Substring(waveText.text.Length - 2).ToString(), out currentWaveNumber);
 
         waveText.text = "WAVE: "+((currentWaveNumber+1).ToString());
+    }
+
+
+    // Update upgrades \\
+    // Changes the upgrade slider alongside the text beside the number \\
+
+    public void UpgradeChange(int upgradeID )
+    {
+        if (upgradeID == 1)
+        {
+            slider1.value = slider1.value + 10 ;
+            slider1Text.text = slider1.value.ToString();
+        }
+        else if (upgradeID == 2)
+        {
+            slider2.value = slider2.value + 10;
+            slider2Text.text = slider2.value.ToString();
+        }
+        else if (upgradeID == 3)
+        {
+            slider3.value = slider3.value + 10;
+            slider3Text.text = slider3.value.ToString();
+        }
+    }
+
+    public void updateUpgrade( int buttonID )
+    {
+
+        // Get current points avaliable 
+
+        int currentPointsAvaliable = getUpgradePointsAvaliable();
+        if (currentPointsAvaliable > 9)
+        {
+            UpgradeChange(buttonID);
+            updateUpgradePointsText(false, 10);
+
+        } 
+    }
+
+    public int getUpgradePointsAvaliable()
+    {
+        // Get current points avaliable and returns an int. \\
+
+        int currentPointsAvaliable;
+        // Gets the last 2 digits of the wave to reduce the amount of selection required.
+        int.TryParse(upgradePoints.text.Substring(upgradePoints.text.Length - 2).ToString(), out currentPointsAvaliable);
+
+        return currentPointsAvaliable;
+    }
+
+
+    public void updateUpgradePointsText(bool addition, int number)
+    {
+        // Updates the text on the upgrades screen. Addition: true = add on the number | Addition: false = remove the number\\
+
+        int currentPointsAvaliable;
+        int upgradeNewNumber;
+
+        // Gets the last 2 digits of the wave to reduce the amount of selection required.
+        int.TryParse(upgradePoints.text.Substring(upgradePoints.text.Length - 2).ToString(), out currentPointsAvaliable);
+
+        if (addition)
+        {
+            upgradeNewNumber = currentPointsAvaliable + number;
+        } else
+        {
+            upgradeNewNumber = currentPointsAvaliable - number;
+        }
+        
+        upgradePoints.text = "Points: " + upgradeNewNumber.ToString();
+
     }
 }
